787f88cea79f2931d8905f1a4718c32426971715
ECIMEX <bayronfernandes@gmail.com>
2022-08-05 15:08:20 -0300

    tarde 05/08/2022

---
+++ JavaScript/Projeto Modulos/funcoes.mjs
@@ -0,0 +1,14 @@
+function mostraIdade(nome, idade){
+    return `A idade de ${nome} é ${idade}`;
+}
+function mostraCidade(nome, cidade){
+    return`A cudade de ${nome} e ${cidade}`;
+}
+function mostraHobby(nome, hobby){
+    return `O hobby de ${nome} é ${hobby}`;
+}
+export{
+    mostraIdade,
+    mostraCidade,
+    mostraHobby
+}
---
+++ JavaScript/Projeto Modulos/main.mjs
@@ -0,0 +1,4 @@
+import { mostraCidade, mostraIdade, mostraHobby} from "./funcoes.mjs";
+console.log(mostraIdade("Bayron", 36));
+console.log(mostraCidade("Bayron", "Palmas"));
+console.log(mostraHobby("Bayron", "Jogar Online MMORPG"));
